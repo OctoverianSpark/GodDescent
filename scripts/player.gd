d1d1ff9d7216974fca8fd7ae9e6c441a62923001
OctoSpark <jeanpaul.hero2@gmail.com>
2025-12-08 18:18:27 -0500

    Config Change

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,8 +3,9 @@ class_name Player
 
 @export var anim_tree: AnimationTree
 @export var animation_states: AnimationNodeStateMachinePlayback
-
+@export var JUMP_SPEED = -300
 @onready var movement: Movement = $Movement as Movement
+@onready var sprite: Sprite2D = $Sprite
 
 enum STATES {
 	IDLE,
@@ -21,32 +22,6 @@ enum STATES {
 var state: STATES = STATES.IDLE
 var dir_x := 0.0  # Solo dirección horizontal
 
-
-func _ready() -> void:
-	movement.setup(self)
-
-
-func _input(event: InputEvent) -> void:
-	dir_x = Input.get_axis("ui_left", "ui_right")
-
-	if dir_x == 0:
-		state = STATES.IDLE
-	else:
-		state = STATES.MOVE
-
-
-func _physics_process(delta: float) -> void:
-	_apply_gravity(delta)
-
-	match state:
-		STATES.MOVE:
-			movement.move(dir_x)
-		_:
-			movement.stop_movement()
-
-	move_and_slide()
-
-
 func _apply_gravity(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += get_gravity().y * delta
@@ -56,5 +31,57 @@ func _apply_gravity(delta: float) -> void:
 
 func _handle_jump():
 	if is_on_floor():
-		velocity.y = -300
+		velocity.y = JUMP_SPEED
 		state = STATES.JUMPING
+		
+func _set_anim(name: String) -> void:
+	if animation_states:
+		animation_states.travel(name)
+	else:
+		print("Playback no asignado")
+
+
+
+func _ready() -> void:
+	movement.setup(self)
+	print(animation_states)
+
+
+
+
+
+func _input(event: InputEvent) -> void:
+	dir_x = Input.get_axis("ui_left", "ui_right")
+
+	if dir_x == 0:
+		state = STATES.IDLE
+	else:
+		state = STATES.MOVE
+	
+	if Input.is_action_just_pressed('accept'):
+		state = STATES.JUMPING
+func _update_anim_state():
+	
+	match state:
+		STATES.IDLE:
+			_set_anim("idle")
+
+		STATES.MOVE:
+			_set_anim("walk")
+
+
+
+func _physics_process(delta: float) -> void:
+	_apply_gravity(delta)
+	_update_anim_state()
+
+	match state:
+		STATES.MOVE:
+			sprite.flip_h = dir_x < 0
+			movement.move(dir_x)
+		STATES.JUMPING:
+			_handle_jump()
+		_:
+			movement.stop_movement()
+
+	move_and_slide()
