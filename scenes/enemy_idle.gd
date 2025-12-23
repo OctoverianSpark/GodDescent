01d261dd392ec3840af85d968dd4d590f1105b45
OctoSpark <jeanpaul.hero2@gmail.com>
2025-12-23 12:16:35 -0500

    Enemy States

---
+++ scenes/enemy_idle.gd
@@ -0,0 +1,16 @@
+class_name EnemyIdle
+
+extends State
+
+func enter() -> void:
+	print("Idle")
+	pass
+
+func exit() -> void:
+	pass
+	
+func update(delta: float) -> void:
+	pass
+	
+func physics_process(delta: float) -> void:
+	pass
